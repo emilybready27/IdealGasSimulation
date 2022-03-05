9ad5571e0dc223244210cb60e44fa9a031c4e2de
emilybready27 <emilybready27@gmail.com>
2022-03-05 13:01:29 -0600

    Added Particle state and methods

--- include/particle.h
+++ include/particle.h
@@ -1,10 +1,28 @@
-#ifndef IDEAL_GAS_PARTICLE_H
-#define IDEAL_GAS_PARTICLE_H
+#pragma once
 
+#include "cinder/gl/gl.h"
 
-class Particle {
+using glm::vec2;
 
-};
+namespace idealgas {
 
+    class Particle {
+    public:
+        Particle(const ci::vec2& position, const ci::vec2& velocity, const float& radius);
 
-#endif //IDEAL_GAS_PARTICLE_H
+        void SetPosition(const ci::vec2& position);
+        ci::vec2 GetPosition() const;
+        void SetVelocity(const ci::vec2& velocity);
+        ci::vec2 GetVelocity() const;
+        void SetRadius(const float& radius);
+        float GetRadius() const;
+
+    private:
+        ci::vec2 position_;
+        ci::vec2 velocity_;
+        float radius_;
+        // later add mass, color
+
+    };
+
+} // namespace idealgas
--- src/particle.cc
+++ src/particle.cc
@@ -1,1 +1,35 @@
 #include "particle.h"
+
+namespace idealgas {
+
+    Particle::Particle(const ci::vec2& position, const ci::vec2& velocity, const float& radius) {
+        position_ = position;
+        velocity_ = velocity;
+        radius_ = radius;
+    }
+
+    void Particle::SetPosition(const ci::vec2& position) {
+        position_ = position;
+    }
+
+    ci::vec2 Particle::GetPosition() const {
+        return position_;
+    }
+
+    void Particle::SetVelocity(const ci::vec2& velocity) {
+        velocity_ = velocity;
+    }
+
+    ci::vec2 Particle::GetVelocity() const {
+        return velocity_;
+    }
+
+    void Particle::SetRadius(const float& radius) {
+        radius_ = radius;
+    }
+
+    float Particle::GetRadius() const {
+        return radius_;
+    }
+
+} // namespace idealgas
