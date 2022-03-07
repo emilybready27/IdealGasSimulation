b1059921cbd7d703b2d415dbc83c9b300bc43bf0
emilybready27 <emilybready27@gmail.com>
2022-03-07 17:24:39 -0600

    Added test for particle collision

--- tests/test_particle.cc
+++ tests/test_particle.cc
@@ -1,103 +1,108 @@
 #include <catch2/catch.hpp>
-
-#include <gas_simulation_app.h>
 #include <particle.h>
 
-using idealgas::IdealGasApp;
 using idealgas::Particle;
 
-static ci::Rectf bounds = ci::Rectf(vec2(100, 100), vec2(775, 775));
-static Particle particle1 = Particle(vec2(775, 775), vec2(-1, -1), 10.0);
-
 TEST_CASE("Handle outside of bound") {
+    ci::Rectf bounds = ci::Rectf(vec2(100, 100), vec2(775, 775));
+    Particle particle = Particle(vec2(775, 775), vec2(-1, -1), 10.0);
+
     SECTION("Particle west of west wall") {
-        particle1.SetPositionX(bounds.x1 - 1);
-        particle1.HandleWallCollision(bounds);
-        REQUIRE(particle1.GetPositionX() == bounds.x1 + particle1.GetRadius());
+        particle.SetPositionX(bounds.x1 - 1);
+        particle.HandleWallCollision(bounds);
+        REQUIRE(particle.GetPositionX() == bounds.x1 + particle.GetRadius());
     }
 
     SECTION("Particle east of east wall") {
-        particle1.SetPositionX(bounds.x2 + 1);
-        particle1.HandleWallCollision(bounds);
-        REQUIRE(particle1.GetPositionX() == bounds.x2 - particle1.GetRadius());
+        particle.SetPositionX(bounds.x2 + 1);
+        particle.HandleWallCollision(bounds);
+        REQUIRE(particle.GetPositionX() == bounds.x2 - particle.GetRadius());
     }
 
     SECTION("Particle north of north wall") {
-        particle1.SetPositionY(bounds.y1 - 1);
-        particle1.HandleWallCollision(bounds);
-        REQUIRE(particle1.GetPositionY() == bounds.y1 + particle1.GetRadius());
+        particle.SetPositionY(bounds.y1 - 1);
+        particle.HandleWallCollision(bounds);
+        REQUIRE(particle.GetPositionY() == bounds.y1 + particle.GetRadius());
     }
 
     SECTION("Particle south of south wall") {
-        particle1.SetPositionY(bounds.y2 + 1);
-        particle1.HandleWallCollision(bounds);
-        REQUIRE(particle1.GetPositionY() == bounds.y2 - particle1.GetRadius());
+        particle.SetPositionY(bounds.y2 + 1);
+        particle.HandleWallCollision(bounds);
+        REQUIRE(particle.GetPositionY() == bounds.y2 - particle.GetRadius());
     }
 
     SECTION("Particle north of north wall and west of west wall") {
-        particle1.SetPosition(vec2(bounds.x1 - 1, bounds.y1 - 1));
-        particle1.HandleWallCollision(bounds);
-        REQUIRE(particle1.GetPosition() == vec2(bounds.x1 + particle1.GetRadius(),
-                                               bounds.y1 + particle1.GetRadius()));
+        particle.SetPosition(vec2(bounds.x1 - 1, bounds.y1 - 1));
+        particle.HandleWallCollision(bounds);
+        REQUIRE(particle.GetPosition() == vec2(bounds.x1 + particle.GetRadius(),
+                                               bounds.y1 + particle.GetRadius()));
     }
 
     SECTION("Particle south of south wall and east of east wall") {
-        particle1.SetPosition(vec2(bounds.x2 + 1, bounds.y2 + 1));
-        particle1.HandleWallCollision(bounds);
-        REQUIRE(particle1.GetPosition() == vec2(bounds.x2 - particle1.GetRadius(),
-                                               bounds.y2 - particle1.GetRadius()));
+        particle.SetPosition(vec2(bounds.x2 + 1, bounds.y2 + 1));
+        particle.HandleWallCollision(bounds);
+        REQUIRE(particle.GetPosition() == vec2(bounds.x2 - particle.GetRadius(),
+                                               bounds.y2 - particle.GetRadius()));
     }
-
 }
 
 TEST_CASE("Handle collision with bound") {
+    ci::Rectf bounds = ci::Rectf(vec2(100, 100), vec2(775, 775));
+    Particle particle = Particle(vec2(775, 775), vec2(-1, -1), 10.0);
+
     SECTION("Particle collision with west wall") {
-        particle1.SetPositionX(bounds.x1 - 1);
-        particle1.SetVelocityX(-1);
-        particle1.HandleWallCollision(bounds);
-        REQUIRE(particle1.GetVelocityX() == 1);
+        particle.SetPositionX(bounds.x1 - 1);
+        particle.SetVelocityX(-1);
+        particle.HandleWallCollision(bounds);
+        REQUIRE(particle.GetVelocityX() == 1);
     }
 
     SECTION("Particle collision with east wall") {
-        particle1.SetPositionX(bounds.x2 + 1);
-        particle1.SetVelocityX(1);
-        particle1.HandleWallCollision(bounds);
-        REQUIRE(particle1.GetVelocityX() == -1);
+        particle.SetPositionX(bounds.x2 + 1);
+        particle.SetVelocityX(1);
+        particle.HandleWallCollision(bounds);
+        REQUIRE(particle.GetVelocityX() == -1);
     }
 
     SECTION("Particle collision with north wall") {
-        particle1.SetPositionY(bounds.y1 - 1);
-        particle1.SetVelocityY(-1);
-        particle1.HandleWallCollision(bounds);
-        REQUIRE(particle1.GetVelocityY() == 1);
+        particle.SetPositionY(bounds.y1 - 1);
+        particle.SetVelocityY(-1);
+        particle.HandleWallCollision(bounds);
+        REQUIRE(particle.GetVelocityY() == 1);
     }
 
     SECTION("Particle collision with south wall") {
-        particle1.SetPositionY(bounds.y2 + 1);
-        particle1.SetVelocityY(1);
-        particle1.HandleWallCollision(bounds);
-        REQUIRE(particle1.GetVelocityY() == -1);
+        particle.SetPositionY(bounds.y2 + 1);
+        particle.SetVelocityY(1);
+        particle.HandleWallCollision(bounds);
+        REQUIRE(particle.GetVelocityY() == -1);
     }
 
     SECTION("Particle collision with northwest corner") {
-        particle1.SetPosition(vec2(bounds.x1 - 1, bounds.y1 - 1));
-        particle1.SetVelocity(vec2(-1, -1));
-        particle1.HandleWallCollision(bounds);
-        REQUIRE(particle1.GetVelocity() == vec2(1, 1));
+        particle.SetPosition(vec2(bounds.x1 - 1, bounds.y1 - 1));
+        particle.SetVelocity(vec2(-1, -1));
+        particle.HandleWallCollision(bounds);
+        REQUIRE(particle.GetVelocity() == vec2(1, 1));
     }
 
     SECTION("Particle collision with southeast corner") {
-        particle1.SetPosition(vec2(bounds.x2 + 1, bounds.y2 + 1));
-        particle1.SetVelocity(vec2(1, 1));
-        particle1.HandleWallCollision(bounds);
-        REQUIRE(particle1.GetVelocity() == vec2(-1, -1));
+        particle.SetPosition(vec2(bounds.x2 + 1, bounds.y2 + 1));
+        particle.SetVelocity(vec2(1, 1));
+        particle.HandleWallCollision(bounds);
+        REQUIRE(particle.GetVelocity() == vec2(-1, -1));
     }
 }
 
-//TEST_CASE("Handle collision with other particle") {
-//    SECTION("Particle in contact with other particle")
-//
-//    SECTION("Particle in contact with and moving towards other particle")
-//
-//    SECTION("Particle collision with itself")
-//}
+TEST_CASE("Handle collision with other particle") {
+    ci::Rectf bounds = ci::Rectf(vec2(100, 100), vec2(775, 775));
+    Particle particle1 = Particle(vec2(775, 775), vec2(-1, -1), 10.0);
+    Particle particle2 = Particle(vec2(775, 775), vec2(-1, -1), 10.0);
+
+    SECTION("Particle collision with another particle") {
+        particle2.SetPosition(bounds.getCenter());
+        particle2.SetVelocity(vec2(1, 1));
+        particle1.SetPosition(particle2.GetPosition() - vec2(1,1));
+        particle1.HandleParticleCollision(particle2);
+        REQUIRE(!particle1.IsMovingTowards(particle2));
+    }
+}
