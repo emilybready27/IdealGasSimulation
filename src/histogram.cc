086e5d43109f0e61a4f6d0377f25b9d86313139b
emilybready27 <emilybready27@gmail.com>
2022-03-17 12:31:53 -0500

    Add weight to frequency values

--- src/histogram.cc
+++ src/histogram.cc
@@ -25,7 +25,7 @@ void Histogram::Display() const {
   for (int i = 0; i < bar_count_; i++) { // only display green particles atm
     ci::gl::color(bar_color_);
     vec2 top_left = vec2(bounds_.x1 + (bar_width_ * (float) i),
-                              bounds_.y2 - (float) (frequencies_[i]));
+                              bounds_.y2 - (float) (frequencies_[i] * 5)); // TODO: magic number
     vec2 bottom_right = vec2(bounds_.x1 + (bar_width_ * (float) (i + 1)),
                                   bounds_.y2);
     ci::gl::drawSolidRect(ci::Rectf(top_left, bottom_right));
