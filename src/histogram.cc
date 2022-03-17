10a28b676599d8ede6150f623bf3868d164ee9eb
emilybready27 <emilybready27@gmail.com>
2022-03-17 11:42:43 -0500

    Got simple histogram to work

--- src/histogram.cc
+++ src/histogram.cc
@@ -6,7 +6,8 @@ idealgas::Histogram::Histogram(const cinder::Rectf &bounds, const cinder::Color 
   bounds_ = bounds;
   bound_color_ = bound_color;
   bar_color_ = particle_color;
-  bar_count_ = particle_count;
+//  bar_count_ = particle_count;
+  bar_count_ = 5; // TODO: add to JSON configuration
   bar_width_ = bounds_.getWidth() / (float) bar_count_;
   frequencies_ = std::vector<int>(bar_count_);
 }
@@ -21,7 +22,7 @@ void idealgas::Histogram::Display() const {
   for (int i = 0; i < bar_count_; i++) { // only display green particles atm
     ci::gl::color(bar_color_);
     vec2 top_left = vec2(bounds_.x1 + (bar_width_ * i),
-                              bounds_.y2 - frequencies_[i]);
+                              bounds_.y2 - (frequencies_[i]));
     vec2 bottom_right = vec2(bounds_.x1 + (bar_width_ * (i + 1)),
                                   bounds_.y2);
     ci::gl::drawSolidRect(ci::Rectf(top_left, bottom_right));
@@ -29,15 +30,22 @@ void idealgas::Histogram::Display() const {
 }
 
 void idealgas::Histogram::AdvanceOneFrame(const std::vector<Particle>& particles) {
-//  std::vector<int> speeds;
-//  for (const Particle& particle : particles) {
-//    speeds.push_back(glm::length(particle.GetVelocity()));
-//  }
-//
-//  std::sort(speeds.begin(), speeds.end());
+  std::vector<float> speeds;
+  for (const Particle& particle : particles) {
+    speeds.push_back(glm::length(particle.GetVelocity()));
+  }
 
-  for (int i = 0; i < bar_count_; i++) {
-    frequencies_[i]++;
+  std::sort(speeds.begin(), speeds.end());
+  float delta = speeds.at(speeds.size() - 1) / bar_count_;
+
+  frequencies_ = std::vector<int>(bar_count_); // reset the frequencies
+  for (size_t i = 0; i < speeds.size(); i++) {
+    for (int j = 0; j < bar_count_; j++) {
+      if (speeds[i] <= delta + (delta * j)) {
+        frequencies_[j]++;
+        break;
+      }
+    }
   }
 }
 
